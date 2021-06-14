ca43d45a893f71834823b85561b9c1901e8b173b
Lachlan Kidson <me@lachlankidson.net>
2021-06-14 19:46:21 +0100

    Normal coloured sphere

--- RayTracingInOneWeekend/Program.cs
+++ RayTracingInOneWeekend/Program.cs
@@ -6,24 +6,34 @@
     {
         public static Vec3 RayColor(Ray ray)
         {
-            if (Program.HitSphere(new Vec3(0, 0, -1), 0.5, ray))
+            Vec3 zm1 = new (0, 0, -1);
+            double t = Program.HitSphere(zm1, 0.5, ray);
+            if (t > 0)
             {
-                return new (1, 0, 0);
+                Vec3 n = Vec3.UnitVector(ray.At(t) - zm1);
+                return 0.5 * new Vec3(n.X + 1, n.Y + 1, n.Z + 1);
             }
 
             Vec3 unitDirection = Vec3.UnitVector(ray.Direction);
-            double t = 0.5 * (unitDirection.Y + 1);
+            t = 0.5 * (unitDirection.Y + 1);
             return ((1 - t) * new Vec3(1, 1, 1)) + (t * new Vec3(0.5, 0.7, 1));
         }
 
-        public static bool HitSphere(Vec3 center, double radius, Ray ray)
+        public static double HitSphere(Vec3 center, double radius, Ray ray)
         {
             Vec3 originCenter = ray.Origin - center;
             double a = Vec3.DotProduct(ray.Direction, ray.Direction);
             double b = 2.0 * Vec3.DotProduct(originCenter, ray.Direction);
-            double c = Vec3.DotProduct(originCenter, originCenter) - (radius * radius);
-            double discriminant = (b * b) - (4 * a * c);
-            return discriminant > 0;
+            double c = Vec3.DotProduct(originCenter, originCenter) - Math.Pow(radius, 2);
+            double discriminant = Math.Pow(b, 2) - (4 * a * c);
+            if (discriminant < 0)
+            {
+                return -1.0;
+            }
+            else
+            {
+                return (-b - Math.Sqrt(discriminant)) / (2.0 * a);
+            }
         }
 
         public static void Main()
